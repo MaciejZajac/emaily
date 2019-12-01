9855badb4b9234ee54e18ee574347ca4f808f3d2
Maciej Zając <maciej.zajac.197@gmail.com>
2019-12-01 18:45:58 +0100

    google authetication

--- index.js
+++ index.js
@@ -1,10 +1,34 @@
 const express = require("express");
-
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const app = express();
+const keys = require("./config/keys");
 
-app.get("/", (req, res) => {
-  res.send({ hi: "lol" });
-});
+// console.developers.google.com
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSercret,
+      callbackURL: "/auth/google/callback"
+    },
+    (accessToken, refreshToken, profile) => {
+      console.log("accessToken", accessToken);
+      console.log("refreshToken", refreshToken);
+      console.log("profile", profile);
+    }
+  )
+);
+
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"]
+  })
+);
+
+app.get("/auth/google/callback", passport.authenticate("google"));
 
 const PORT = process.env.PORT || 5000;
 
